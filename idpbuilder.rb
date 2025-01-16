e7b0cbe2fbc9de1b2eb6c130d1b48cf5c68d3a80
Charles Moulliard <cmoulliard@redhat.com>
2025-01-16 18:00:40 +0100

    Update idpbuilder.rb

--- idpbuilder.rb
+++ idpbuilder.rb
@@ -1,1 +1,9 @@
-// idpbuilder.rb
+class Idpbuilder < Formula
+  desc "IDP Client to create kind cluster."
+  homepage "https://cnoe.io/"
+  url "https://github.com/cnoe-io/idpbuilder.git",
+      tag:      "v0.8.1",
+      revision: "8c6cd9704ae118981a89b3c5b541db1924f31643"
+  license "Apache-2.0"
+  head "https://github.com/cnoe-io/idpbuilder.git", branch: "main"
+end  
