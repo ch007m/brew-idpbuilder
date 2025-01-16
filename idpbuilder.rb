9c811ab57ad10a304d78765eb9dd7c4c51aaed4e
Charles Moulliard <cmoulliard@redhat.com>
2025-01-16 18:33:39 +0100

    Update idpbuilder.rb

--- idpbuilder.rb
+++ idpbuilder.rb
@@ -6,4 +6,23 @@ class Idpbuilder < Formula
       revision: "8c6cd9704ae118981a89b3c5b541db1924f31643"
   license "Apache-2.0"
   head "https://github.com/cnoe-io/idpbuilder.git", branch: "main"
-end  
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/cnoe-io/idpbuilder/pkg/cmd/version.idpbuilderVersion=#{version} \
+      -X github.com/cnoe-io/idpbuilder/pkg/cmd/version.gitCommit=#{Utils.git_head} \
+      -X github.com/cnoe-io/idpbuilder/pkg/cmd/version.buildDate=#{time.strftime("%F")}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"idpbuilder", "completion")
+  end
+
+  test do
+    assert_match "Idpbuilder is a CLI to manage your IDP clusters.",
+                 shell_output("#{bin}/idpbuilder -h")
+  end
+end
