250b4c5d5ea55d305b414af327dbd237c30a414e
cmoulliard <cmoulliard@redhat.com>
2025-01-17 13:36:48 +0100

    Created the formula to be used with ch007m org
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- idpbuilder.rb
+++ idpbuilder.rb
@@ -1,9 +1,8 @@
 class Idpbuilder < Formula
-  desc "IDP Client to create kind cluster."
+  desc "IDP Client to create kind cluster"
   homepage "https://cnoe.io/"
-  url "https://github.com/cnoe-io/idpbuilder.git",
-      tag:      "v0.8.1",
-      revision: "8c6cd9704ae118981a89b3c5b541db1924f31643"
+  url "https://github.com/cnoe-io/idpbuilder/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "5fe24d7449d596d573d90b140cc4e946ae3c691e63b06ec3d7d7547ee2274b0a"
   license "Apache-2.0"
   head "https://github.com/cnoe-io/idpbuilder.git", branch: "main"
 
